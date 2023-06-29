d0773ca7fadf326ecf0706fb7a1a6920a8beea06
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-06-30 03:05:43 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -29,6 +29,7 @@ all_builds = [
     "sony_headphones_connect",
     "reddit_boost",
     "google_recorder",
+    "nova_launcher",
 ]
 possible_archs = ["armeabi-v7a", "x86", "x86_64", "arm64-v8a"]
 
