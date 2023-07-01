953d11eff95b3862175f1f2ea57f840e62d4db9f
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-07-01 23:06:39 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 # Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_arguments("--no-sandbox");
+chrome_options.add_arguments("--disable-dev-shm-usage");
 
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome(options=chrome_options)
