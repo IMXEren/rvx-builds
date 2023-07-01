a56d4846038ff9674e898f046926760d682f796b
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-07-01 22:07:50 +0530

    Create test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to a website
+driver.get("https://www.example.com")
+
+# Find an element by its CSS selector and interact with it
+element = driver.find_element(By.CSS_SELECTOR, "h1")
+print(element.text)
+
+# Close the browser
+driver.quit()
