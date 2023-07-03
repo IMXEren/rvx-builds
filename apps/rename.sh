ddff9f3b8a1f0b635354e303908ebb7376b0dc37
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-07-04 03:16:02 +0530

    Update and rename rename.sed to rename.sh

---
+++ apps/rename.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+github_repo="$1"
+rename_script_1="$2"
+
+# Replace "py_file_url = ..." with a new URL
+sed -i "s@py_file_url = "https://raw.githubusercontent.com/.*@py_file_url = "https://raw.githubusercontent.com/$github_repo/main/src/patches.py"@" "$rename_script_1"
+
+# Replace "config_py_file_url = ..." with a new URL
+sed -i "s@config_py_file_url = "https://raw\.githubusercontent\.com/.*@config_py_file_url = "https://raw.githubusercontent.com/$github_repo/main/src/config.py"@" "$rename_script_1"
+
+# Add more sed commands if needed
