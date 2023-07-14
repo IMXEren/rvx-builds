a0b8d40d14acdb8b0f64586ff67b2f0e04f287f6
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-07-14 21:12:04 +0530

    Update rename.sh

--- apps/scripts/rename.sh
+++ apps/scripts/rename.sh
@@ -2,12 +2,12 @@
 
 github_repo="IMXEren/rvx-builds"
 rename_script_1="apps/scripts/apps.py"
-# rename_script_2
+rename_script_2="apps/scripts/utils/repo.py"
 
 # Replace "py_file_url = ..." with a new URL
-sed -i "s@py_file_url = \"https://raw.githubusercontent.com/.*@py_file_url = \"https://raw.githubusercontent.com/$github_repo/main/src/patches.py\"@" "$rename_script_1"
+sed -i "s@repo = .*@repo = \"$github_repo\"@" "$rename_script_2"
 
 # Replace "config_py_file_url = ..." with a new URL
-sed -i "s@config_py_file_url = \"https://raw\.githubusercontent\.com/.*@config_py_file_url = \"https://raw.githubusercontent.com/$github_repo/main/src/config.py\"@" "$rename_script_1"
+# sed -i "s@config_py_file_url = \"https://raw\.githubusercontent\.com/.*@config_py_file_url = \"https://raw.githubusercontent.com/$github_repo/main/src/config.py\"@" "$rename_script_1"
 
 # Add more sed commands if needed
