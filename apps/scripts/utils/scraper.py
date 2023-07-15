a7bddedc790967e6705bc1bc5c6d2fe90478cfd6
IMXEren <vickrey_readings@simplelogin.com>
2023-07-15 19:22:57 +0530

    Update scraper.py

--- apps/scripts/utils/scraper.py
+++ apps/scripts/utils/scraper.py
@@ -50,11 +50,7 @@ def apkm_scrape(package_name, app_code):
         s = requests
         hdr = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Encoding': 'none',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            }
+        }
         r = s.get(app_url, headers=hdr)
         soup = BeautifulSoup(r.text, "html.parser")
         app_name_element = soup.select_one("#masthead > header > div > div > div.f-grow > h1")
