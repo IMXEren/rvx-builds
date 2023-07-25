7c19611661d23f89c19e248bd0626858fddf9406
IMXEren <vickrey_readings@simplelogin.com>
2023-07-25 23:36:40 +0530

    Update parse-env-json.py

--- apps/scripts/parse-env-json.py
+++ apps/scripts/parse-env-json.py
@@ -72,6 +72,8 @@ def parse_json_data(env_content):
         line = line.strip()
         if not line or line.startswith('#') or line.isspace():
             continue
+        if '#' in line:
+            line = line.split('#')[0].strip()
         key, value = line.split('=', 1)
         key = key.strip()
         value = value.strip()
