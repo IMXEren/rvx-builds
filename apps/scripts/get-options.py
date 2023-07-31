26f2216b156fa5c1b58d2552805be6553759bb25
IMXEren <vickrey_readings@simplelogin.com>
2023-07-31 19:08:09 +0530

    Update logging in scripts

--- apps/scripts/get-options.py
+++ apps/scripts/get-options.py
@@ -2,11 +2,13 @@ import os
 import re
 import json
 import requests
+from loguru import logger
 
 from utils.repo import GitHubRepo
 from utils.urls import GitHubURLs
 
 # Get patches JSON
+@logger.catch
 def get_options_json(url):
     response = requests.get(url)
     patches_json = response.text
@@ -29,39 +31,42 @@ def get_options_json(url):
         if obj_tuple not in unique_objects:
             unique_objects.add(obj_tuple)
             options_json.append({"patchName": patch_name, "options": options_list})
-    print(options_json)
     return options_json
 
 # Convert to formatted JSON string
+@logger.catch
 def format_options_json(opjson):
     opjson_str = json.dumps(opjson, indent=1, separators=(",", " : "), ensure_ascii=False)
     opjson_str = re.sub(r'\[\n(?:(?:\s+?)?)\{', r'[ {', opjson_str) # [ {
     opjson_str = re.sub(r' \}\n(?:(?:\s+?)?)\]', r'} ]', opjson_str) # } ]
     opjson_str = re.sub(r' \},\n\s+?\{', r'}, {', opjson_str) # }, {
-    print(opjson_str)
     return opjson_str
 
-gh = GitHubRepo()
-repo = gh.get_repo()
-branch = gh.get_branch()
-urls = GitHubURLs(repo, branch)
-rv_json_url = urls.get_rv_json()
-rvx_json_url = urls.get_rvx_json()
+if __name__ == "__main__":
+    gh = GitHubRepo()
+    repo = gh.get_repo()
+    branch = gh.get_branch()
+    urls = GitHubURLs(repo, branch)
+    rv_json_url = urls.get_rv_json()
+    rvx_json_url = urls.get_rvx_json()
 
-urls = [
-    rv_json_url,
-    rvx_json_url,
-]
+    urls = [
+        rv_json_url,
+        rvx_json_url,
+    ]
 
-outs = [
-    "apps/revanced/options.json",
-    "apps/revanced-extended/options.json",
-]
+    outs = [
+        "apps/revanced/options.json",
+        "apps/revanced-extended/options.json",
+    ]
 
-for url, output_file in zip(urls, outs):
-    options_json = get_options_json(url)
-    # options_json_str = json.dumps(options_json, indent=2)
-    options_json_str = format_options_json(options_json)
-    os.makedirs(os.path.dirname(output_file), exist_ok=True) # Create the directories if they don't exist
-    with open(output_file, "w") as file:
-        file.write(options_json_str)
+    for url, output_file in zip(urls, outs):
+        options_json = get_options_json(url)
+        logger.debug(options_json)
+        # options_json_str = json.dumps(options_json, indent=2)
+        options_json_str = format_options_json(options_json)
+        logger.debug(options_json_str)
+        os.makedirs(os.path.dirname(output_file), exist_ok=True) # Create the directories if they don't exist
+        with open(output_file, "w") as file:
+            file.write(options_json_str)
+        logger.info('Fetched options.json for revanced and revanced-extended')
