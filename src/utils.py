dbec01bdf6732d462079651a4a8a73a906632949
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-10-10 19:34:02 +0530

    chore: bump browserforge and selenium-driverless

--- src/utils.py
+++ src/utils.py
@@ -49,7 +49,7 @@ changelog_json_file = "changelog.json"
 request_timeout = 60
 request_retries = 15
 session = Session()
-_browsers = [Browser("chrome", min_version=119, max_version=122)]
+_browsers = [Browser("chrome", min_version=130, max_version=140)]
 _headers = HeaderGenerator(browser=_browsers, os="linux", device="desktop").generate()
 _headers.pop("Accept-Encoding", None)
 request_header.update(_headers)
