bb70fc311460a106a2eca0e61e1d9322ea9b746e
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-10-17 23:48:11 +0530

    t3

--- src/browser/test.py
+++ src/browser/test.py
@@ -7,9 +7,11 @@ from bs4 import BeautifulSoup
 from loguru import logger
 
 sys.path.append(str(Path.cwd()))
-# from src.browser.site import source as page_source
+from src.browser.site import source as page_source
+
 # import nodriver as uc
-import zendriver as uc
+
+# import zendriver as uc
 
 
 async def main() -> None:  # noqa: D103
@@ -18,24 +20,24 @@ async def main() -> None:  # noqa: D103
     # logger.info("Started display")  # noqa: ERA001
 
     url = "https://www.wikipedia.com/"  ## Redirect test
-    url = "https://nowsecure.nl"  ## Cloudflare
     url = "https://bot.sannysoft.com/"  ## AntiBot validator
     url = "https://fingerprintjs.github.io/BotD"  ## AntiBot validator
     url = "https://community.cloudflare.com/t/bot-traffic-managed-to-bypass-cloudflare-interactive-challenge-captcha/541364"  ## Cloudflare  # noqa: E501
     url = "https://nopecha.com/demo"  ## Cloudflare
     url = "https://www.apkmirror.com/apk/facebook-2/facebook/facebook-490-0-0-release/"
+    url = "https://nowsecure.nl"  ## Cloudflare
 
     try:
-        # r = await page_source(url)
-        browser = await uc.start()
-        page = await browser.get(url)
-        await page.verify_cf()
-        r = await page.get_content()
-        soup = BeautifulSoup(r, "html.parser")
+        r = await page_source(url)
+        # browser = await uc.start()
+        # page = await browser.get(url)
+        # await page.verify_cf()
+        # r = await page.get_content()
+        soup = BeautifulSoup(r.text, "html.parser")
         element = soup.select_one("title")
         if element:
             print(element.text)  # noqa: T201
-        await browser.stop()
+        # await browser.stop()
     except Exception as e:  # noqa: BLE001
         logger.error(e)
 
