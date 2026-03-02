5d1a431af5f3bb271df04cc12f298cbfda1c125a
IMXEren <96839938+IMXEren@users.noreply.github.com>
2026-03-02 22:16:14 +0530

    fix: remove headers sent to ip-geo api

--- src/browser/ip_geo.py
+++ src/browser/ip_geo.py
@@ -55,12 +55,6 @@ class IPGeolocationInfo:
             r = requests.get(
                 "https://ipapi.co/json/",
                 timeout=10,
-                headers={
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                    "AppleWebKit/537.36 (HTML, like Gecko)"
-                    " Chrome/143.0.0.0 Safari/537.36",
-                    "Accept": "application/json",
-                },
             )
             r = r.json()
             if r.get("error", False):
